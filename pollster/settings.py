03181d7579d47faf5ff27435aaa0e713bfa7aa3c
VinCoD <5895.2020@students.ku.ac.ke>
2022-07-28 18:47:11 +0300

    Adding django on heroku

--- pollster/settings.py
+++ pollster/settings.py
@@ -120,3 +120,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Heroku Settings.
+import django_on_heroku
+
+django_on_heroku.settings(locals())
+
